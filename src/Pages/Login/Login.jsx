b13acb2fbd0b2ac17d965963bd60fcce6ecafbf4
aadilllkkkhhhaaannn <hamzakhannoor76@gmail.com>
2025-09-01 21:22:28 +0530

    state

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,6 +6,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const [to , setTo] = useState('')
 
   // to login
   const handleLogin = (e) => {
